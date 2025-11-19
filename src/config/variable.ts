e739db80a2db3a8900e9d2dec394aee9c36ce7e0
Nghuy2001 <Nguyennquangghuy@gmail.com>
2025-11-19 16:34:49 +0700

    Trang chi tiết CV (Nhà tuyển dụng) - FE

--- src/config/variable.ts
+++ src/config/variable.ts
@@ -39,3 +39,21 @@ export const workingFormList = [
     value: "parttime"
   },
 ];
+
+export const cvStatusList = [
+  {
+    label: "Chưa duyệt",
+    value: "initial",
+    color: "#121212"
+  },
+  {
+    label: "Đã duyệt",
+    value: "approved",
+    color: "#47BE02"
+  },
+  {
+    label: "Từ chối",
+    value: "rejected",
+    color: "#FF5100"
+  }
+];
